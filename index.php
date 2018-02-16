bcf3b0b0096a46ece0e0f9b20fdc4de05f25b6c6
wswapped <wswapped@gmail.com>
2018-02-16 16:06:40 +0200

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="aqvert team">
+        <title>AquaVert | Welcome</title>
+    </head>
+<body>
+    Welcome to AquaVert    
+</body>
+
+</html>
